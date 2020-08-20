f9293e113e7b3c6a60e9be7d0f09ecf823085a44
Priyanshu Mehta <mehta7priyanshu@gmail.com>
2020-08-20 17:32:04 +0530

    Day 20

---
+++ Day 20/ReorderList.java
@@ -0,0 +1,38 @@
+package august2020;
+
+import july2020.RemoveLinkedListElements.ListNode;
+
+public class ReorderList {
+
+	public void reorderList(ListNode head) {
+
+		if (head == null)
+			return;
+
+		ListNode slow = head, fast = head;
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+
+		ListNode prev = null, curr = slow, tmp;
+		while (curr != null) {
+			tmp = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = tmp;
+		}
+
+		ListNode first = head, second = prev;
+		while (second.next != null) {
+			tmp = first.next;
+			first.next = second;
+			first = tmp;
+
+			tmp = second.next;
+			second.next = first;
+			second = tmp;
+		}
+	}
+
+}
